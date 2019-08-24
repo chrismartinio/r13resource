a0503a04f9291f5437059f9d68840931116b9a78
Chris Martin <chris@sensedev.com>
2019-08-24 09:28:01 -0700

    scheduler

--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from data import get_lectures
+from requests import requests
+from bs4 import BeautifulSoup
+from models import Lecture
 
 from models import db
 from app import app
@@ -9,9 +13,34 @@ manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
 
+
 @manager.command
-def hello():
+def get_all_lectures():
+
+    soup = get_lectures()
+    links = []
+    titles = []
+
+    for link in soup.find_all('a'):
+        links.append('http://curric.rithmschool.com/r13/lectures/' +
+                     link.get('href'))
+
+    for link in links:
+        if 'zip' in link:
+            continue
+        response = requests.get(link)
+        soup = BeautifulSoup(response.text)
+        if (soup.title is None):
+            continue
+        else:
+            new_lecture = Lecture(title=soup.title.string, url=link)
+            db.session.add(new_lecture)
+            titles.append(soup.title.string)
+
+    db.session.commit()
+
     print('hello')
+
 
 if __name__ == '__main__':
     manager.run()
