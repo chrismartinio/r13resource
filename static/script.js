12e914cddea753d0415918773ac919c067ce9241
Chris Martin <chris@sensedev.com>
2019-08-24 19:49:11 -0700

    fix

--- static/script.js
+++ static/script.js
@@ -10,28 +10,46 @@ document.addEventListener('DOMContentLoaded', function () {
 
   $('.collapsible').collapsible();
 
+  $(function () {
+    $(".lecture-link").on("click", function (e) {
+      e.preventDefault(); 
+      let id = $(this).data('lecture-id');
+      let url = $(this).data('lecture-url');
+      $('#main').html(`<iframe src='${url}'></iframe>`);
+      $(`#${id}`).addClass('active');
+    });
+  });
+  
+  $(function () {
+    $(".exercise-link").on("click", function (e) {
+      e.preventDefault(); 
+      let id = $(this).data('exercise-id');
+      let url = $(this).data('exercise-url');
+      $('#main').html(`<iframe src='${url}'></iframe>`);
+      $(`#${id}`).addClass('active');
+    });
+  });
+
   console.log('loaded');
 
-
-
 });
 
-$(function () {
-  $(".lecture-link").on("click", function (e) {
-    e.preventDefault(); // cancel the link itself
-    let id = $(this).data('lecture-id');
-    let url = $(this).data('lecture-url');
-    $('#main').html(`<iframe src='${url}'></iframe>`);
-    $(`#${id}`).addClass('active');
-  });
-});
+// $(function () {
+//   $(".lecture-link").on("click", function (e) {
+//     e.preventDefault(); 
+//     let id = $(this).data('lecture-id');
+//     let url = $(this).data('lecture-url');
+//     $('#main').html(`<iframe src='${url}'></iframe>`);
+//     $(`#${id}`).addClass('active');
+//   });
+// });
 
-$(function () {
-  $(".exercise-link").on("click", function (e) {
-    e.preventDefault(); // cancel the link itself
-    let id = $(this).data('exercise-id');
-    let url = $(this).data('exercise-url');
-    $('#main').html(`<iframe src='${url}'></iframe>`);
-    $(`#${id}`).addClass('active');
-  });
-});
+// $(function () {
+//   $(".exercise-link").on("click", function (e) {
+//     e.preventDefault(); 
+//     let id = $(this).data('exercise-id');
+//     let url = $(this).data('exercise-url');
+//     $('#main').html(`<iframe src='${url}'></iframe>`);
+//     $(`#${id}`).addClass('active');
+//   });
+// });
