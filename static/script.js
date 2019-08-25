cb1725644667b8a763e6fe085a5ecdb024cee9d5
Chris Martin <chris@sensedev.com>
2019-08-24 20:21:36 -0700

    fixes

--- static/script.js
+++ static/script.js
@@ -10,48 +10,26 @@ document.addEventListener('DOMContentLoaded', function () {
 
   $('.collapsible').collapsible();
 
-  $(function () {
-    $(".lecture-link").on("click", function (e) {
-      e.preventDefault(); 
-      console.log('click')
-      let id = $(this).data('lecture-id');
-      let url = $(this).data('lecture-url');
-      $('#main').html(`<iframe src='${url}'></iframe>`);
-      $(`#${id}`).addClass('active');
-    });
-  });
-  
-  $(function () {
-    $(".exercise-link").click(function(e) {
-      e.preventDefault(); 
-      console.log('click')
-      let id = $(this).data('exercise-id');
-      let url = $(this).data('exercise-url');
-      $('#main').html(`<iframe src='${url}'></iframe>`);
-      $(`#${id}`).addClass('active');
-    });
-  });
-
   console.log('loaded');
 
 });
 
-// $(function () {
-//   $(".lecture-link").on("click", function (e) {
-//     e.preventDefault(); 
-//     let id = $(this).data('lecture-id');
-//     let url = $(this).data('lecture-url');
-//     $('#main').html(`<iframe src='${url}'></iframe>`);
-//     $(`#${id}`).addClass('active');
-//   });
-// });
+$(function () {
+  $(".lecture-link").on("click", function (e) {
+    e.preventDefault(); 
+    let id = $(this).data('lecture-id');
+    let url = $(this).data('lecture-url');
+    $('#main').html(`<iframe src='${url}'></iframe>`);
+    $(`#${id}`).addClass('active');
+  });
+});
 
-// $(function () {
-//   $(".exercise-link").on("click", function (e) {
-//     e.preventDefault(); 
-//     let id = $(this).data('exercise-id');
-//     let url = $(this).data('exercise-url');
-//     $('#main').html(`<iframe src='${url}'></iframe>`);
-//     $(`#${id}`).addClass('active');
-//   });
-// });
+$(function () {
+  $(".exercise-link").on("click", function (e) {
+    e.preventDefault(); 
+    let id = $(this).data('exercise-id');
+    let url = $(this).data('exercise-url');
+    $('#main').html(`<iframe src='${url}'></iframe>`);
+    $(`#${id}`).addClass('active');
+  });
+});
